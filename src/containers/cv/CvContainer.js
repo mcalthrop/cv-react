de3a77cb6c7583eda477880611d7bd0a643f58b6
Matt Calthrop <matt.calthrop@redacademy.com>
2018-09-10 18:20:06 +0100

    Add Contentful; interim commit

--- src/containers/cv/CvContainer.js
+++ src/containers/cv/CvContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Education, OnTheWeb, OtherInterests, Overview, WorkHistory } from '../../components';
+import * as contentful from 'contentful';
 
 const data = {
     overview: {
@@ -138,8 +139,35 @@ const data = {
         ],
     },
 }
+
+const space = '8nmbnmef5jq7';
+const accessToken = '2b0af88ae087bef7c88668c9a00cd778656e22d726598b797a246415c9176eaf';
+
 export class CvContainer extends Component {
+    state = {
+        data: {},
+    };
+
+    contentfulClient = contentful.createClient({ space, accessToken });
+
+    processEntries(entries) {
+        console.log('processEntries(): entries:', entries);
+        this.setState({
+            data: entries,
+        });
+    }
+
+    componentDidMount() {
+        const config = {
+            content_type: 'cv',
+            include: 2,
+        };
+
+        this.contentfulClient.getEntries(config).then(this.processEntries.bind(this));
+    }
+
     render() {
+        console.log('render(): this.state:', this.state);
         return (
             <header>
                 <h1>CV: Matt Calthrop</h1>
