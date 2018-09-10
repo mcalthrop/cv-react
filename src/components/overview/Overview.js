bb89ee7fd109f892402a46ef4a6d09914f140c03
Matt Calthrop <matt.calthrop@redacademy.com>
2018-09-10 11:22:00 +0100

    Add Overview component

---
+++ src/components/overview/Overview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Overview = () => {
+    return (
+        <section>
+            <h2>Overview</h2>
+            <p>para 1</p>
+            <p>para 2</p>
+        </section>
+    );
+};
--- src/containers/cv/CvContainer.js
+++ src/containers/cv/CvContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Overview } from '../../components/overview';
 
 export class CvContainer extends Component {
     render() {
         return (
             <header>
                 <h1>CV: Matt Calthrop</h1>
+                <Overview></Overview>
             </header>
         );
     }
