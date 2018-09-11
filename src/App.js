619b8bada2d4f92ded09a4a97828476dd65a5c51
Matt Calthrop <matt.calthrop@redacademy.com>
2018-09-11 11:11:06 +0100

    Add DataTransformation class

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import { CvContainer } from './containers/cv';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <CvContainer></CvContainer>
-      </div>
+      <CvContainer></CvContainer>
     );
   }
 }
--- src/containers/cv/CvContainer.js
+++ src/containers/cv/CvContainer.js
@@ -1,160 +1,22 @@
 import React, { Component } from 'react';
 import { Education, OnTheWeb, OtherInterests, Overview, WorkHistory } from '../../components';
+import { DataTransformation } from '../../utils/DataTransformation';
 import * as contentful from 'contentful';
-
-const data = {
-    overview: {
-        paragraphs: [
-            'Matt Calthrop is a highly experienced full-stack software engineer and trainer with 25 years\' experience in software analysis, development and systems integration.',
-            'With an aptitude for teaching and mentoring, Matt\'s professional and conscientious approach ensures timely product delivery.',
-        ],
-    },
-    onTheWeb: {
-        items: [
-            {
-                title: 'Source code to my online CV',
-                link: 'https://github.com/mcalthrop/cv#readme',
-            },
-            {
-                title: 'Express server with MongoDB and authentication',
-                link: 'https://github.com/mcalthrop/auth-mongo-express-node#readme',
-            },
-            {
-                title: 'TDD implementation of JavaScript Fibonacci series function',
-                link: 'https://github.com/mcalthrop/tdd-fibonacci#readme',
-            },
-        ],
-    },
-    workHistory: {
-        items: [
-            {
-                company: {
-                    title: 'RED Academy',
-                    link: 'https://redacademy.com/london/',
-                },
-                jobTitle: 'Lead Instructor, Application Development',
-                location: 'London, UK',
-                duration: {
-                    start: 'June 2018',
-                    finish: 'present',
-                },
-                paragraphs: [
-                    'Teaching web application development. Coaching and mentoring. Imparting what I\'ve learned over the last 25 years. Preparing students for software development in the real world.',
-                ],
-            },
-            {
-                company: {
-                    title: 'Experian',
-                    link: 'http://www.experian.co.uk/',
-                },
-                jobTitle: 'Senior Front-end Consultant',
-                location: 'London, UK',
-                duration: {
-                    start: 'July 2017',
-                    finish: 'June 2018',
-                },
-                paragraphs: [
-                    'Building a greenfield Angular application. Requirements gathering and consolidation.',
-                    'Automated unit, component and e2e testing.',
-                    'On-site and remote teaching and mentoring of Angular, web components, and coding best practices.',
-                ],
-                skills: [
-                    'Angular',
-                    'Web components',
-                    'TypeScript',
-                    'HTML5',
-                    'CSS3',
-                    'Bootstrap',
-                    'Webpack',
-                    'Test automation',
-                    'Karma',
-                    'Protractor',
-                    'Mocha',
-                    'Chai',
-                ],
-            },
-            {
-                company: {
-                    title: 'Ada College',
-                    link: 'https://ada.ac.uk/',
-                },
-                jobTitle: 'Lecturer, Apprenticeship Programme',
-                location: 'London, UK',
-                duration: {
-                    start: 'May 2017',
-                    finish: 'June 2017',
-                },
-                paragraphs: [
-                    'Writing and delivering course material for the 8-week launchpad of Ada\'s Apprenticeship programme for two Open University-approved modules: Software Development Lifecycle (focusing on Agile methodologies) and Testing (focusing on automation and CI).',
-                ],
-                skills: [
-                    'Training',
-                    'Mentoring',
-                    'Course-writing',
-                    'Coaching',
-                    'Java',
-                    'jUnit',
-                    'Maven',
-                    'Selenium',
-                    'Travis',
-                ],
-            },
-            {
-                company: {
-                    title: 't5media',
-                },
-                jobTitle: 'Systems Engineer',
-                location: 'St Albans, UK',
-                duration: {
-                    start: '2002',
-                    finish: '2007',
-                },
-                paragraphs: [
-                    'Project manager, systems engineer, and website developer building websites for SMEs.',
-                    'Managed LAMP stack hosting of all websites, including Bash script automation of website creation and backups.',
-                    'Requirements analysis and specification, project management, hosting configuration, implementation and support.',
-                ],
-                skills: [
-                    'Full software lifecycle',
-                    'Systems integration',
-                    'LAMP implementation',
-                    'HTML',
-                    'CSS',
-                    'JavaScript',
-                    'Client liaison and support',
-                ],
-            },
-        ],
-    },
-    education: {
-        paragraphs: [
-            '1992: Australian National University, Canberra, Australia – Bachelor of Computer Science',
-            '1986: Christ Church Grammar School, Perth, Australia – TEE score of 402/510',
-        ],
-    },
-    otherInterests: {
-        paragraphs: [
-            'I have invested in various charitable causes, including building a primary school in Nicaragua, financially supporting a family in Uganda who fosters AIDS orphans, and being involved locally in the distribution of food and basics.',
-            'Additionally, I love spending time with my family, in the outdoors, mountain biking and running.',
-        ],
-    },
-}
 
 const space = '8nmbnmef5jq7';
 const accessToken = '2b0af88ae087bef7c88668c9a00cd778656e22d726598b797a246415c9176eaf';
 
 export class CvContainer extends Component {
     state = {
-        data: {},
+        data: undefined,
     };
 
     contentfulClient = contentful.createClient({ space, accessToken });
 
     processEntries(entries) {
-        console.log('processEntries(): entries:', entries);
-        this.setState({
-            data: entries,
-        });
+        const data = DataTransformation.transformCv(entries);
+
+        this.setState({ data });
     }
 
     componentDidMount() {
@@ -167,15 +29,24 @@ export class CvContainer extends Component {
     }
 
     render() {
+        const { data } = this.state;
         console.log('render(): this.state:', this.state);
         return (
             <header>
                 <h1>CV: Matt Calthrop</h1>
-                <Overview paragraphs={data.overview.paragraphs} />
-                <OnTheWeb items={data.onTheWeb.items} />
-                <WorkHistory items={data.workHistory.items} />
-                <Education paragraphs={data.education.paragraphs} />
-                <OtherInterests paragraphs={data.otherInterests.paragraphs} />
+                {data &&
+                    <div>
+                        <Overview paragraphs={data.overview.paragraphs} />
+                        <OnTheWeb items={data.onTheWeb.items} />
+                        <WorkHistory items={data.workHistory.items} />
+                        <Education paragraphs={data.education.paragraphs} />
+                        <OtherInterests paragraphs={data.otherInterests.paragraphs} />
+                    </div>
+                    ||
+                    <div>
+                        <p>Loading...</p>
+                    </div>
+                }
             </header>
         );
     }
