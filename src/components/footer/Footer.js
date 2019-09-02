8d6bbd6069c0861567dad6f980d40324b1bc94e8
Matt Calthrop <mcalthrop@gmail.com>
2019-09-02 21:27:44 +0100

    Footer now shows copyright year range instead of fixed year

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,9 +3,12 @@ import { FullWidthRow } from '../';
 import './Footer.css';
 
 export const Footer = () => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
   return (
     <FullWidthRow>
-      <div className="footer mui--text-dark-hint">Copyright &copy; Matt Calthrop 2018</div>
+      <div className="footer mui--text-dark-hint">Copyright &copy; Matt Calthrop 2018-{currentYear}</div>
     </FullWidthRow>
   );
 };
